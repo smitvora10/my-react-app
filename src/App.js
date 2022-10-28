1bfbb4e47a2a326175597bbf1499d768d324b59e
smitvora <smitvora10@gmail.com>
2022-10-28 10:04:59 +0530

    string changed commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is s demo website
         </a>
       </header>
     </div>
